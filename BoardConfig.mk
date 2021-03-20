fef25d00a9b2a181ffef6a1b928556b6eded6ff5
Ferdinand Vasquez <killcmd@gmail.com>
2021-03-20 01:26:17 -0400

    Update BoardConfig.mk
    changed defconfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -56,7 +56,7 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 TARGET_KERNEL_ADDITIONAL_FLAGS := DTC_EXT=$(shell pwd)/prebuilts/misc/linux-x86/dtc/dtc
 TARGET_KERNEL_SOURCE := kernel/oneplus/sm6350
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CONFIG := vendor/billie-perf_defconfig
+TARGET_KERNEL_CONFIG := vendor/lito-perf_defconfig
 
 # Platform
 BOARD_USES_QCOM_HARDWARE := true
